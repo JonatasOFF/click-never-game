731495967957b314aecfcb53344c94327cf75b98
JonatasOFF <jonatasoliveiraferreira158@gmail.com>
2023-03-10 00:46:57 -0300

    fix menu play in center

--- src/levels/Menu.js
+++ src/levels/Menu.js
@@ -5,28 +5,25 @@ import { Zen } from "./Zen";
 export class Menu {
   constructor(app) {
     this.container = new Container();
-
+    
     const buttonSprite = new Sprite(Assets.get("play"));
 
-    buttonSprite.position = new Point(
-      window.innerWidth / 2,
-      window.innerHeight / 2
-    );
+    buttonSprite.position = new Point(370, window.innerHeight / 2);
     const button = new Button(buttonSprite);
 
     button.onPress.connect(() => {
-      this.modeZen()
+      this.modeZen();
     });
 
     this.container.addChild(button.view);
     this.app = app;
     this.app.stage.addChild(this.container);
+    
+    
   }
 
-
-
   modeZen() {
-    const zen = new Zen(this.app)
+    const zen = new Zen(this.app);
     this.app.stage.addChild(zen.container);
   }
 }
